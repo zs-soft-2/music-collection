5a875417f74b797674aed450c609cb1114dd15a7
zsagia <zsagia@gmail.com>
2022-10-18 09:27:45 +0200

    Generate project by nx cli #2

---
+++ apps/music-collection/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'music-collection-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'music-collection';
+}
