5769acab9b70d1ab5711bd1568ee7ab4fdd92827
zsagia <zsagia@gmail.com>
2022-10-19 17:54:35 +0200

    Create an authorization library#7

--- apps/music-collection/src/app/app.module.ts
+++ apps/music-collection/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreAuthenticationModule } from '@music-collection/core/authentication';
+import { CoreAuthorizationModule } from '@music-collection/core/authorization';
 import { DomainUserModule } from '@music-collection/domain/user';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NgxPermissionsModule } from 'ngx-permissions';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
@@ -27,7 +29,9 @@ import { TopBarModule } from './module';
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([]),
     TopBarModule,
+    NgxPermissionsModule.forRoot(),
     CoreAuthenticationModule,
+    CoreAuthorizationModule,
     DomainUserModule,
   ],
   providers: [],
---
+++ libs/core/authorization/README.md
@@ -0,0 +1,7 @@
+# core-authorization
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test core-authorization` to execute the unit tests.
