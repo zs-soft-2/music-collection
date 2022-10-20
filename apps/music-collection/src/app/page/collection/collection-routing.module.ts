c80e9cbc845391a4294ed0d119a7d4aa715d8696
zsagia <zsagia@gmail.com>
2022-10-20 15:05:00 +0200

    Create a collection page #12

---
+++ apps/music-collection/src/app/page/collection/collection-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CollectionComponent } from './collection.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: CollectionComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CollectionRoutingModule {}
