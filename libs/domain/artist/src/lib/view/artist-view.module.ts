a126fef7a3312469ed7908d90aa88ac0ecef5fc7
zsagia <zsagia@gmail.com>
2022-10-30 09:35:24 +0100

    Add design changes

--- libs/domain/artist/src/lib/view/artist-view.module.ts
+++ libs/domain/artist/src/lib/view/artist-view.module.ts
@@ -1,9 +1,9 @@
-import { CardModule } from 'primeng/card';
 import { ChipModule } from 'primeng/chip';
 import { ImageModule } from 'primeng/image';
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import {
 	ArtistDetailViewComponent,
@@ -13,6 +13,6 @@ import {
 @NgModule({
 	exports: [ArtistDetailViewComponent, ArtistSimpleViewComponent],
 	declarations: [ArtistDetailViewComponent, ArtistSimpleViewComponent],
-	imports: [CommonModule, CardModule, ChipModule, ImageModule],
+	imports: [CommonModule, ChipModule, FlexLayoutModule, ImageModule],
 })
 export class ArtistViewModule {}
