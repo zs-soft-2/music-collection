d00fd5138b5ae23760bb8e503646680d99d1ea1b
zsagia <zsagia@gmail.com>
2022-11-06 15:48:01 +0100

    Implement collection page #42

--- apps/music-collection/src/app/page/collection/collection.module.ts
+++ apps/music-collection/src/app/page/collection/collection.module.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CollectionItemCollectionModule } from '@music-collection/domain/collection-item';
 
 import { CollectionRoutingModule } from './collection-routing.module';
 import { CollectionComponent } from './collection.component';
 
 @NgModule({
 	declarations: [CollectionComponent],
-	imports: [CommonModule, CollectionRoutingModule],
+	imports: [
+		CommonModule,
+		CollectionRoutingModule,
+		CollectionItemCollectionModule,
+	],
 })
 export class CollectionModule {}
